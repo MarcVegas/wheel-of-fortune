38f774e06b3669fc55bf02bc4c860eac5a9c2f9d
AJ Almaguer <ajalmaguer@gmail.com>
2016-03-18 18:08:38 -0700

    place clue function has working word wrap

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ console.log("script loaded")
 var tiles = [$(".tile.r1"),$(".tile.r2"),$(".tile.r3")]
 var tilesPerRow = tiles[0].length
 
-var clue = "hello my name is aj"
+var clue = "hello my name is bob"
 
 function placeClue() {
   clue = clue.split(" ")
@@ -11,16 +11,21 @@ function placeClue() {
   var currCol = 0
 
   for(var i=0; i<clue.length; i++) {  //for each element (aka word) in clue
-    currWordLenth = clue[i]
-    if(currCol + currWordLenth < tilesPerRow) { //if element fits
+    currWord = clue[i]
+    if(currCol + currWord.length < tilesPerRow) { //if element fits
       if(currCol !== 0) { //if not on first column add a space
         currCol += 1
       }
     } else {
       currRow += 1 //go to next row
+      currCol = 0 //go to first tile of that row
+    }
+    for(var j=0; j< currWord.length; j++) {
+      $(tiles[currRow][currCol]).addClass('blank-tile').html(currWord[j])
+      currCol++
     }
   }
-
+}
   //for each element (aka word) in clue
      //check if the word fits
         //if it fits
@@ -29,13 +34,6 @@ function placeClue() {
        // else go to next row
      //now add the word
 
-     for (var j=0;j<currWord.length; j++) {
-       $(tiles[currentTile]).html(currWord[j])
-       console.log(currWord[j], currentTile)
-       currentTile++
-     }
-
-}
 
 
 
